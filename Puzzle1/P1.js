f4f58c10935ef822ace142d4b6485cc07dcdac54
Tanmay Gujar <tanmaygujar999@gmail.com>
2020-12-03 09:59:45 +0530

    refactor

--- Puzzle1/P1.js
+++ Puzzle1/P1.js
@@ -1,3 +1,5 @@
+// Puzzle available at https://adventofcode.com/2020/day/1
+
 const fs = require('fs');
 try {  
     let data = fs.readFileSync('input.txt', 'utf8');
@@ -6,11 +8,15 @@ try {
     console.log('Error:', e.stack);
 }
 
+// assuming entries are unique
 function findParts(entries, sum, splits) {
     let part;
+    // if only one number is needed, find it in the array
     if (splits === 1) return entries.includes(sum) ? [sum] : null;
+
     for (let entry of entries) {
-        if (part = findParts(entries, sum - entry, splits - 1)) {
+        if (part = findParts(entries.filter(e => e !== entry), sum - entry, splits - 1)) {
+            // select an entry and find other parts which complete the sum
             return [entry, ...part]
         }
     }
