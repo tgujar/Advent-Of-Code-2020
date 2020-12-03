058edd9ac9279702c4cfa91b2748f26d4b3cfea5
Tanmay Gujar <tanmaygujar999@gmail.com>
2020-12-03 11:56:23 +0530

    refactor P2

--- Puzzle2/P2.js
+++ Puzzle2/P2.js
@@ -2,31 +2,36 @@
 
 const fs = require('fs');
 let input;
-try {  
+try {
     const data = fs.readFileSync('input.txt', 'utf8');
-    input = data.toString().split("\n");    
-} catch(e) {
+    input = data.toString().split("\n");
+} catch (e) {
     console.log('Error:', e.stack);
 }
 
 // get parameters for each row
 const rows = input.map(row => {
     [lower, upper, char, pass] = row.split(/-|:\s|\s/);
-    return {lower, upper, char, pass};
+    return { lower, upper, char, pass };
 })
 
-const validOld = rows.reduce((acc,row) => {
-    const matches = row.pass.match(new RegExp(row.char, "g")); // get matches
+function countCorrect(rows, isValid) {
+    return rows.reduce((acc, row) => {
+        if (isValid(row)) return acc + 1;
+        return acc;
+    }, 0)
+}
+
+const validOld = ({ lower, upper, char, pass }) => {
+    const matches = pass.match(new RegExp(char, "g")); // get matches
     // check if number of matches is within bounds
-    if (matches && matches.length <= row.upper && matches.length >= row.lower) return acc + 1;
-    return acc;
-}, 0);
+    return matches && matches.length <= upper && matches.length >= lower;
+};
 
-const validNew = rows.reduce((acc,{upper, lower, char, pass}) => {
+const validNew = ({ lower, upper, char, pass }) => {
     // Xor returns true if only one of them is true
-    if (pass[lower - 1] == char ^ pass[upper - 1] == char) return acc + 1;
-    return acc;
-}, 0);
+    return pass[lower - 1] == char ^ pass[upper - 1] == char
+}
 
-console.log(validOld); // part1
-console.log(validNew); // part2
+console.log(countCorrect(rows, validOld)); // part1
+console.log(countCorrect(rows, validNew)); // part2
