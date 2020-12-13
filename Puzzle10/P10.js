8aa505aa5d829d566b7f8acf3110a28e9e62d489
Tanmay Gujar <tanmaygujar999@gmail.com>
2020-12-13 22:39:27 +0530

    bug fixes and refactor

--- Puzzle10/P10.js
+++ Puzzle10/P10.js
@@ -7,14 +7,7 @@ try {
     console.log('Error:', e.stack);
 }
 
-const allowedJTGDiffs = [1, 2, 3]; // joltage differences allowed
 const deviceJoltage = Math.max(...adapters) + 3;
-
-// calculates factorial
-function fact(n) {
-    if (n === 0) return 1
-    return n * fact(n - 1);
-}
 
 // counts the number of occurances of elem in arr
 const countElem = (arr, elem) => arr.filter(i => i === elem).length;
@@ -29,55 +22,20 @@ function getJoltageDiffs(adapters) {
     return diff.slice(0, diff.length - 1); // remove the last value, which is NaN
 }
 
-// count number of ways in which the "total" can be expresses as a sum of "allowed"
-function countChange(totalJTG, allowed, JTGs = []) {
-    const sum = JTGs.reduce((a, b) => a + b, 0);
-    if (sum === totalJTG) { // sum of JTGs is same as that required
-        /* find number of combinations of the JTGs, i.e if JTGs = [1,2,1,3],
-        then combinations of this is also equal to sum */
-        return countCombinations(JTGs);  
-    } else if (sum > totalJTG || allowed.length === 0) {
-        return 0;
-    } else {
-        return countChange(totalJTG, allowed, JTGs.concat(allowed[0])) // add value to JTGs, and rerun
-            + countChange(totalJTG, allowed.slice(1), JTGs); // count combination of totalJTG expressed as subset of allowed
+// referenced from http://tiny.cc/pim6tz, changed my solution because this was much more elegant
+function countCombos(adapters) {
+    let mem = new Map().set("0", 1);
+    adapters.sort((a,b) => a - b);
+    adapters.push(adapters[adapters.length - 1] + 3);
+    for (let a of adapters) {
+        mem.set(String(a), (mem.get(String(a-1)) || 0) + 
+                        (mem.get(String(a-2)) || 0) + 
+                        (mem.get(String(a-3)) || 0));
     }
-}
-
-// count the number of ways an array can be rearranged to get a new array
-function countCombinations(arr) {
-    const unique = new Set(arr); // get unique entries in arr
-    const nCr = (n, r) => fact(n) / (fact(n - r) * fact(r)); // n!/(n-r)!r!
-    let combinations = 1;
-    let counted = 0;
-
-    // if arr = [1,2,3,3,5,5,5] then calculate 7C3*4C2*2P1*1P1
-    for (let num of unique) {
-        let occurances = arr.filter(elem => elem === num).length;
-        combinations *= nCr(arr.length - counted, occurances);
-        counted += occurances;
-    }
-    return combinations;
-}
-
-function countJTGCombinations(JTGDiffs) {
-    return JTGDiffs
-            .join("")
-            // split into consecutive numbers, e.g "111331" will become ["111", "33", "1"]
-            .match(new RegExp(allowedJTGDiffs.map(elem => `${elem}+`).join("|"), "g")) 
-            .reduce((acc, patt) => {
-                let q = Number(patt[0]); // get the number in the sequence
-                /*
-                The number can only be replaced by a higher number since we cant add more adapters,
-                We are essentially counting the number of ways the total value i.e patt.length * q
-                can be split up as sum of allowedJTGDiffs greater than or equal the number in the sequence.
-                Then we multiply it with previous calculated combinations (acc)
-                */
-                return acc * countChange(patt.length * q, allowedJTGDiffs.filter(e => e >= q));
-            }, 1)
+    return mem.get(String(adapters[adapters.length - 1]));
 }
 
 const diffs = getJoltageDiffs(adapters.concat(0, deviceJoltage));
 
 console.log((countElem(diffs, 3)) * countElem(diffs, 1)); // part 1
-console.log(countJTGCombinations(diffs)); // part 2
+console.log(countCombos(adapters)); // part 2
